605bd3188d36757e370d75008a4f4fda44f2d43d
Folasade Agbaje <folasadeagbaje@gmail.com>
2020-02-12 15:40:37 +0100

    create room class

--- src/player.py
+++ src/player.py
@@ -1,2 +1,10 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+class Room:
+  def __init__(self, name, description):
+    self.name = name 
+    self.description = description
+    self.n_to = None  
+    self.s_to = None
+    self.e_to = None
+    self.w_to = None
