45a4e272ced17a7b6a679649ce1213fce66c4034
Folasade Agbaje <folasadeagbaje@gmail.com>
2020-02-13 13:52:05 +0100

    create basic item class

---
+++ src/item.py
@@ -0,0 +1,8 @@
+#creating the item class with name and description attributes
+
+class Item:
+  def __init__(self, name, description):
+    self.name = name
+    self.description = description
+
+  
